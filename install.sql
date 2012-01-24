f4880528511b9a6682ada39c5199d6028fbc55fc
Dyllon Mahan <dmahan67@gmail.com>
2012-01-24 10:03:58 -0500

    added hooks support

--- install.sql
+++ install.sql
@@ -19,9 +19,19 @@ CREATE TABLE `groups` (
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5;
 
-
 INSERT INTO `groups` (`id`, `title`, `desc`, `icon`) VALUES
 (1, 'Inactive', 'Inactive', ''),
 (2, 'Banned', 'Banned', ''),
 (3, 'Members', 'Members', ''),
 (4, 'Administrators', 'Administrators', '');
+
+CREATE TABLE `plugins` (
+  `id` mediumint NOT NULL auto_increment,
+  `hook` varchar(64) collate utf8_unicode_ci NOT NULL default '',
+  `code` varchar(64) collate utf8_unicode_ci NOT NULL default '',
+  `part` varchar(32) collate utf8_unicode_ci NOT NULL default '',
+  `title` varchar(255) collate utf8_unicode_ci NOT NULL default '',
+  `file` varchar(255) collate utf8_unicode_ci NOT NULL default '',
+  `active` tinyint unsigned NOT NULL default '1',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
